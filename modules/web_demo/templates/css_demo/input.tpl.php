f1c91163a94eaea568c27dd18538c38f33cd7cbc
Jun Ni <jun.ni@tanan.net.cn>
2018-01-29 03:48:19 +0800

    moha 7.x-1.0-dev

---
+++ modules/web_demo/templates/css_demo/input.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * @Created: 01/12/2017 5:32 AM
+ */
+
+?>
+<label for="name">Name: </label>
+<div class="input_box_wrapper">
+  <input id="name" title="" type="text" class="gradient_border_text_input" autocomplete="off"/>
+</div>
+
+<label for="gender">Gender: </label>
+<div class="input_box_wrapper_round">
+  <input id="gender" title="" type="text" class="gradient_border_text_input"/>
+</div>
+
+
