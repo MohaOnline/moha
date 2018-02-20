904ea5f651c247b271c2bfb171f07e7059fa7012
Jun Ni <jun.ni@tanan.net.cn>
2018-02-20 20:15:29 +0800

    moha 7.x-1.0-dev
    - Fix URL for CKEditor assets from HTTPS.
    - Move layout of moha-spotlight to common style file.
    - Add style for hint area.

--- modules/moha_clip/ckeditor/templates.js
+++ modules/moha_clip/ckeditor/templates.js
@@ -1,16 +1,28 @@
 
+var hint_area_template = '';
+hint_area_template += '<div class="moha-spotlight hint-area">';
+hint_area_template += '<div class="moha-spotlight-icon hint-icon fa fa-info"><span>&nbsp;</span></div>';
+hint_area_template += '<div class="moha-spotlight-details hint-details"><p>&nbsp;</p>';
+hint_area_template += '<p>&nbsp;</p></div>';
+hint_area_template += '</div>';
+
 var reference_links_template = '';
-reference_links_template += '<div class="reference-area">';
-reference_links_template += '<div class="reference-icon fa fa-external-link fa-2x"><span>&nbsp;</span></div>';
-reference_links_template += '<div class="reference-details"><p>&nbsp;</p>';
+reference_links_template += '<div class="moha-spotlight reference-area">';
+reference_links_template += '<div class="moha-spotlight-icon reference-icon fa fa-external-link"><span>&nbsp;</span></div>';
+reference_links_template += '<div class="moha-spotlight-details reference-details"><p>&nbsp;</p>';
 reference_links_template += '<p>&nbsp;</p></div>';
 reference_links_template += '</div>';
-
 
 CKEDITOR.addTemplates('default',
   {
     imagesPath: Drupal.settings.moha_clip.ckeditor_template_image_path,
     templates: [
+      {
+        title: 'Hint block',
+        image: 'reference-block.png',
+        description: 'Gives hint messages.',
+        html: hint_area_template
+      },
       {
         title: 'Reference block',
         image: 'reference-block.png',
