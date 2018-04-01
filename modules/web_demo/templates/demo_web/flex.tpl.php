5f0863b9a94d15c2943a87ea043523e23c8f46eb
Jun Ni <jun.ni@tanan.net.cn>
2018-04-02 02:44:51 +0800

    moha 7.x-1.0-dev
    - Sync WeChat Welcome message.

---
+++ modules/web_demo/templates/demo_web/flex.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * @Created: 28/03/2018 10:04 PM
+ */
+?>
+
+<div class="flex-column">
+  <div class="flex-item">1</div>
+  <div class="flex-item">2<br/>2<br/>2</div>
+  <div class="flex-item">3</div>
+  <div class="flex-item">4</div>
+  <div class="flex-item">5</div>
+  <div class="flex-item">6</div>
+  <div class="flex-item">7</div>
+  <div class="flex-item">8</div>
+</div>
