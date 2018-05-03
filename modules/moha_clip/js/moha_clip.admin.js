4c17c1492eacba2dbcc90b0112fc582345e8d141
Jun Ni <jun.ni@tanan.net.cn>
2018-05-03 10:51:35 +0800

    moha-1.0-dev
    - Hide success Drupal message automatically.

--- modules/moha_clip/js/moha_clip.admin.js
+++ modules/moha_clip/js/moha_clip.admin.js
@@ -2,6 +2,11 @@
 
   Drupal.behaviors.moha_clip_admin = {
     attach: function (context, settings) {
+      // Hide success message automatically.
+      $(".messages.status", context).once('success-message-fade-out', function(){
+        $(this).delay(5000).fadeTo(1500, 0.05).slideUp(500);
+      });
+
       // Add collapsible effect for image and video field.
       $('form', context).once('collapsed', function () {
         var $fieldset = $(this).find('.field-type-image fieldset, .field-type-video fieldset');
