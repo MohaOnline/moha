253b659abe58a4c2e8ecad4b6dea51b1d2c1748a
Jun Ni <jun.ni@tanan.net.cn>
2018-05-10 23:51:58 +0800

    moha 7.x-1.0-dev
    - Moha Clip: fix toolbar.

--- modules/moha_clip/js/moha_clip.admin.js
+++ modules/moha_clip/js/moha_clip.admin.js
@@ -31,6 +31,38 @@
 
           }
         }, false);
+
+        // Fix CKEditor toolbar.
+        window.addEventListener('scroll', function(){
+          const content             = document.getElementsByClassName('cke_contents').item(1);
+          const toolbar             = document.getElementsByClassName('cke_top').item(1);
+          const editor              = document.getElementsByClassName('cke').item(1);
+          const inner               = document.getElementsByClassName('cke_inner').item(1);
+          const scrollvalue         = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+
+          toolbar.style.width     = content.offsetWidth + "px";
+          toolbar.style.top       = "0px";
+          toolbar.style.margin    = "0 auto";
+          toolbar.style.boxSizing = "border-box";
+
+          if(toolbar.offsetTop <= scrollvalue){
+            toolbar.style.top       = "29px";
+            toolbar.style.position   = "fixed";
+            content.style.paddingTop = toolbar.offsetHeight + "px";
+          }
+
+          if(editor.offsetTop > scrollvalue && (editor.offsetTop + editor.offsetHeight) >= (scrollvalue + toolbar.offsetHeight)){
+            toolbar.style.position   = "relative";
+            content.style.paddingTop = "0px";
+          }
+
+          if((editor.offsetTop + editor.offsetHeight) < (scrollvalue + toolbar.offsetHeight)){
+            toolbar.style.position = "absolute";
+            toolbar.style.top      = "calc(100% - " + toolbar.offsetHeight + "px)";
+            inner.style.position   = "relative";
+            content.style.paddingTop = "0px";
+          }
+        }, false);
       });
 
     }
