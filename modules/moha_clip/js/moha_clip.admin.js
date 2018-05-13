0ce43ef54456daab0b02c69048dc72b1d69b1f06
Jun Ni <jun.ni@tanan.net.cn>
2018-05-13 19:18:47 +0800

    moha 7.x-1.0-dev
    - Moha Clip: fix toolbar - considering header height.

--- modules/moha_clip/js/moha_clip.admin.js
+++ modules/moha_clip/js/moha_clip.admin.js
@@ -34,29 +34,33 @@
 
         // Fix CKEditor toolbar.
         window.addEventListener('scroll', function(){
+          const headerHeight        = document.getElementById('branding').offsetHeight;
           const content             = document.getElementsByClassName('cke_contents').item(1);
           const toolbar             = document.getElementsByClassName('cke_top').item(1);
+          const toolbarAfterAdminY  = '29px';
           const editor              = document.getElementsByClassName('cke').item(1);
           const inner               = document.getElementsByClassName('cke_inner').item(1);
-          const scrollvalue         = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+          // Current scrolled distance.
+          const currentScrollY      = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
 
           toolbar.style.width     = content.offsetWidth + "px";
           toolbar.style.top       = "0px";
           toolbar.style.margin    = "0 auto";
           toolbar.style.boxSizing = "border-box";
 
-          if(toolbar.offsetTop <= scrollvalue){
-            toolbar.style.top       = "29px";
+          // Scroll over editor, so fix toolbar after admin menu.
+          if(currentScrollY > headerHeight + editor.offsetTop){
+            toolbar.style.top        = toolbarAfterAdminY;
             toolbar.style.position   = "fixed";
             content.style.paddingTop = toolbar.offsetHeight + "px";
           }
 
-          if(editor.offsetTop > scrollvalue && (editor.offsetTop + editor.offsetHeight) >= (scrollvalue + toolbar.offsetHeight)){
+          if(currentScrollY <= headerHeight + editor.offsetTop ){
             toolbar.style.position   = "relative";
             content.style.paddingTop = "0px";
           }
 
-          if((editor.offsetTop + editor.offsetHeight) < (scrollvalue + toolbar.offsetHeight)){
+          if((editor.offsetTop + editor.offsetHeight) < (currentScrollY + toolbar.offsetHeight)){
             toolbar.style.position = "absolute";
             toolbar.style.top      = "calc(100% - " + toolbar.offsetHeight + "px)";
             inner.style.position   = "relative";
