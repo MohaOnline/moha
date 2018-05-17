8a00c10525032769ece7ee95a13dc0b00bffdf2d
Jun Ni <jun.ni@tanan.net.cn>
2018-05-18 04:09:10 +0800

    moha 7.x-1.0-dev
    - Moha WeChat: fix typo.

--- modules/moha_message/templates/moha_message_wall_say_thank_you.tpl.php
+++ modules/moha_message/templates/moha_message_wall_say_thank_you.tpl.php
@@ -254,11 +254,11 @@
             imgUrl: 'https://gestorage.blob.core.chinacloudapi.cn/campaign/20180515/message-icon.jpg',
             success: function () {
               // 用户确认分享后执行的回调函数
-              alert('分享到朋友圈成功');
+              //alert('分享到朋友圈成功');
             },
             cancel: function () {
               // 用户取消分享后执行的回调函数
-              alert('你没有分享到朋友圈');
+              //alert('你没有分享到朋友圈');
             }
           });
           wx.onMenuShareAppMessage({
@@ -270,13 +270,13 @@
               // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
             },
             success: function (res) {
-              alert('分享给朋友成功');
+              //alert('分享给朋友成功');
             },
             cancel: function (res) {
-              alert('你没有分享给朋友');
+              //alert('你没有分享给朋友');
             },
             fail: function (res) {
-              alert(JSON.stringify(res));
+              //alert(JSON.stringify(res));
             }
           });
         });
