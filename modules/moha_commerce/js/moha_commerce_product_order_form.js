f08cef39c0aa4ef472726b2fa023ab9b4ed551b5
Jun Ni <jun.ni@tanan.net.cn>
2018-06-17 14:21:35 +0800

    moha 7.x-1.0-dev
    - Moha Commerce: Close order page in WeChat.
    - Moha Commerce Order: Prepare comment fields.
    - Moha Commerce Order: Fix profile2 missing issue.
    - Moha Commerce Order: Update default delete behavior.
    - Moha WX: add closeWindow to Approved JS SDK.

--- modules/moha_commerce/js/moha_commerce_product_order_form.js
+++ modules/moha_commerce/js/moha_commerce_product_order_form.js
@@ -4,6 +4,12 @@
     $('#form-cancel').click(function (event){
       event.preventDefault();
       window.close();
+
+      // @see https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115 10.3.
+      if (wx !== undefined) {
+        wx.closeWindow();
+      }
+
     });
 
   });
--- modules/moha_commerce/modules/moha_commerce_order/includes/MohaCommerceOrderEntityController.php
+++ modules/moha_commerce/modules/moha_commerce_order/includes/MohaCommerceOrderEntityController.php
@@ -3,6 +3,117 @@
  * @file
  */
 
+/**
+ * Class MohaCommerceOrderEntityController
+ *
+ * CRUD support for moha_commerce_order entity.
+ */
 class MohaCommerceOrderEntityController extends EntityAPIController{
+  /**
+   * Builds a structured array representing the entity's content.
+   *
+   * The content built for the entity will vary depending on the $view_mode
+   * parameter.
+   *
+   * @param $entity object
+   *   An entity object.
+   * @param $view_mode string
+   *   View mode, e.g. 'full', 'teaser'...
+   * @param $langcode string
+   *   (optional) A language code to use for rendering. Defaults to the global
+   *   content language of the current request.
+   * @param $content array
+   *   Optionally. Allows pre-populating the built content to ease overridding
+   *   this method.
+   *
+   * @return array
+   *   The renderable array, keyed by entity name or numeric id.
+   */
+  public function buildContent($entity, $view_mode = 'full', $langcode = NULL, $content = array()) {
+
+    $build = parent::buildContent($entity, $view_mode, $langcode, $content);
+
+
+
+    return $build;
+  }
+
+  /**
+   * Implements EntityAPIControllerInterface.
+   *
+   * Update entity status instead of really delete them.
+   *
+   * @param $ids array
+   *   Entity ID array.
+   * @param $transaction
+   *   Optionally a DatabaseTransaction object to use. Allows overrides to pass
+   *   in their transaction object.
+   *
+   * @throws \Exception
+   */
+  public function delete($ids, DatabaseTransaction $transaction = NULL) {
+    $entities = $ids ? $this->load($ids) : FALSE;
+    if (!$entities) {
+      // Do nothing, in case invalid or no ids have been passed.
+      return;
+    }
+    $transaction = isset($transaction) ? $transaction : db_transaction();
+
+    try {
+      $ids = array_keys($entities);
+
+
+      if (isset($this->revisionTable)) {
+        // Save revision.
+      }
+
+      db_update($this->entityInfo['base table'])
+        ->fields(array(
+          'status' => 0,
+          'updated' => REQUEST_TIME,
+        ))
+        ->condition($this->idKey, $ids, 'IN')
+        ->execute();
+
+      // Reset the cache as soon as the changes have been applied.
+      $this->resetCache($ids);
+
+      // Ignore slave server temporarily.
+      db_ignore_slave();
+    }
+    catch (Exception $e) {
+      $transaction->rollback();
+      watchdog_exception($this->entityType, $e);
+      throw $e;
+    }
+  }
+
+  /**
+   * Implements EntityAPIControllerInterface.
+   *
+   * Set entity saved time.
+   *
+   * @param $entity
+   *   The entity to save.
+   * @param $transaction
+   *   Optionally a DatabaseTransaction object to use. Allows overrides to pass
+   *   in their transaction object.
+   *
+   * @return integer
+   *   SAVED_NEW | SAVED_UPDATED
+   *
+   * @throws \Exception
+   *   In case of failures.
+   */
+  public function save($entity, DatabaseTransaction $transaction = NULL) {
+
+    if (isset($entity->is_new)){
+      $entity->created = REQUEST_TIME;
+    }
+
+    $entity->updated = REQUEST_TIME;
+
+    return parent::save($entity, $transaction);
+  }
 
 }
