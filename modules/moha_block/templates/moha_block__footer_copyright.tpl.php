ee1025f740c4979ff2feed210add38c992223043
Jun Ni <jun.ni@tanan.net.cn>
2018-07-12 18:38:14 +0800

    moha 7.x-1.0-dev
    - Moha Block: Footer Copyright

---
+++ modules/moha_block/templates/moha_block__footer_copyright.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * Template file for Moha Block: Footer - Copyright.
+ */
+?>
+<div class="moha-block-bottom-copyright moha-block-black-bottom-2017"><?php print($contents['copyright']); ?></div>
