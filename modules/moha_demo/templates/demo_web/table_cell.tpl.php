69925662e7ab4493ae2e32a68b10f1e5ca8c104b
Jun Ni <jun.ni@tanan.net.cn>
2018-07-20 10:31:56 +0800

    moha 7.x-1.0-dev
    - Moha Demo: table-cell middle

---
+++ modules/moha_demo/templates/demo_web/table_cell.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ */
+?>
+<pre>
+外部容器 div（wrapper div） 设置成： display: table;
+需要垂直居中的 div 设置成： display: table-cell; vertical-align: middle;
+</pre>
+
+<div class="table-cell-wrapper">
+  <div class="table-cell-vertical-middle"><img src="http://via.placeholder.com/350x150"></div>
+</div>
+
