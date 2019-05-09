1380be6a3d9742f2ba5cd5c76fd3696e60eee7fd
Jun Ni <jun.ni@tanan.net.cn>
2019-05-09 10:04:52 +0800

    moha 7.x-1.0-dev
    - moha_sms: update phpdoc.
    - moha_dashboard: update path CONSTs.
    - moha_demo: update path
    - moha_its: remove view UI dependency
    - moha_ui: module skeleton

---
+++ modules/moha_demo/templates/demo_web/real_time_preview.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file template for demo online preview.
+ */
+/** @var array $page */
+?>
+<iframe name="moha_demo_real_time_preview" id="moha_demo_real_time_preview" class="iphone_x_portrait" frameborder="0" src="<?php echo $page['preview_url']; ?>">
+  <p></p>
+</iframe>
+
