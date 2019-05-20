d7c5829faf1938a6d5759078057b0d569d527ba2
Jun Ni <jun.ni@tanan.net.cn>
2019-05-21 04:15:16 +0800

    moha 7.x-1.0-dev
    - moha_block: rebuild block content with render array.

--- modules/moha_block/templates/moha_block__footer_copyright.tpl.php
+++ modules/moha_block/templates/moha_block__footer_copyright.tpl.php
@@ -2,6 +2,8 @@
 /**
  * @file
  * Template file for Moha Block: Footer - Copyright.
+ *
+ * @see _moha_block_footer_copyright_contents().
  */
 ?>
-<div class="moha-block-bottom-copyright moha-block-black-bottom-2017"><?php print($contents['copyright']); ?></div>
+<div class="moha-block-bottom-copyright moha-block-black-bottom-2017"><?php /** @var $contents array*/ print($contents['copyright']); ?></div>
--- modules/moha_block/templates/moha_block__footer_icp.tpl.php
+++ modules/moha_block/templates/moha_block__footer_icp.tpl.php
@@ -1,4 +1,12 @@
+<?php
+/**
+ * @file
+ * Template file for Moha Block: Footer - ICP.
+ *
+ * @see _moha_block_footer_icp_contents().
+ */
+?>
 <div class="moha-footer-license">
-  <a href="http://www.miitbeian.gov.cn/"
-     target="_blank" rel="noopener noreferrer"><?php print($contents['icp']); ?></a>
+  <a href="http://www.beian.miit.gov.cn"
+     target="_blank" rel="noopener noreferrer"><?php /** @var $contents array*/ print($contents['icp']); ?></a>
 </div>
