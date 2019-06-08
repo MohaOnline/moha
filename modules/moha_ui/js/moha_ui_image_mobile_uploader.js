7656ec292d0b7202e42d43f89615830723c9f093
Jun Ni <jun.ni@tanan.net.cn>
2019-06-08 17:06:09 +0800

    moha_ui 7.x-1.0-dev #3.3
    - Input file styled.

---
+++ modules/moha_ui/js/moha_ui_image_mobile_uploader.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ */
+
+(function ($) {
+  /* jshint unused:false, strict: false */
+  /* globals Drupal:object */
+  Drupal.behaviors.mohaUIImageMobileUploader = {
+    attach: function(context) {
+
+    }
+  };
+
+})(jQuery);
