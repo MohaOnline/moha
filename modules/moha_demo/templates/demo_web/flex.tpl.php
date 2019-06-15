647876f5e65152c8776ed9640b587ccda6e0d2f8
Jun Ni <tweeg.ni@gmail.com>
2019-06-15 20:11:03 +0800

    moha_demo 7.x-1.0-dev
    - #6 Add horizontal scroll demo with flex.

--- modules/moha_demo/templates/demo_web/flex.tpl.php
+++ modules/moha_demo/templates/demo_web/flex.tpl.php
@@ -1,16 +1,71 @@
 <?php
 /**
  * @file
+ *   Moha Flexbox Demo.
+ *
+ * @see moha_demo.flex.css
  */
 ?>
+<h2>flex row</h2>
+<div class="flex-container">
+  <div class="flex-item"><span> 1 </span></div>
+  <div class="flex-item"><span> 2 </span></div>
+  <div class="flex-item"><span> 3 </span></div>
+  <div class="flex-item"><span> 4 </span></div>
+  <div class="flex-item"><span> 5 </span></div>
+  <div class="flex-item"><span> 6 </span></div>
+  <div class="flex-item"><span> 7 </span></div>
+  <div class="flex-item"><span> 8 </span></div>
+  <div class="flex-item"><span> 9 </span></div>
+  <div class="flex-item"><span> 10 </span></div>
+  <div class="flex-item"><span> 11 </span></div>
+  <div class="flex-item"><span> 12 </span></div>
+  <div class="flex-item"><span> 13 </span></div>
+  <div class="flex-item"><span> 14 </span></div>
+  <div class="flex-item"><span> 15 </span></div>
+</div>
 
-<div class="flex-column">
-  <div class="flex-item">1</div>
-  <div class="flex-item">2<br/>2<br/>2</div>
-  <div class="flex-item">3</div>
-  <div class="flex-item">4</div>
-  <div class="flex-item">5</div>
-  <div class="flex-item">6</div>
-  <div class="flex-item">7</div>
-  <div class="flex-item">8</div>
+<h2>Horizontal scroll: item 25%</h2>
+<div class="flex-container-horizontal-scroll">
+  <div class="flex-item"><span> 1 </span></div>
+  <div class="flex-item"><span> 2 </span></div>
+  <div class="flex-item"><span> 3 </span></div>
+  <div class="flex-item"><span> 4 </span></div>
+  <div class="flex-item"><span> 5 </span></div>
+  <div class="flex-item"><span> 6 </span></div>
+  <div class="flex-item"><span> 7 </span></div>
+  <div class="flex-item"><span> 8 </span></div>
+  <div class="flex-item"><span> 9 </span></div>
+  <div class="flex-item"><span> 10 </span></div>
+  <div class="flex-item"><span> 11 </span></div>
+  <div class="flex-item"><span> 12 </span></div>
+  <div class="flex-item"><span> 13 </span></div>
+  <div class="flex-item"><span> 14 </span></div>
+  <div class="flex-item"><span> 15 </span></div>
+</div>
+
+<h2>Horizontal scroll: no enough items</h2>
+<div class="flex-container-horizontal-scroll">
+  <div class="flex-item"><span> 1 </span></div>
+  <div class="flex-item"><span> 2 </span></div>
+</div>
+
+
+<h2>Vertical scroll</h2>
+<div class="flex-container-column">
+  <div class="flex-item"><span> 1 </span></div>
+  <div class="flex-item"><span> 2 </span></div>
+  <div class="flex-item"><span> 3 </span></div>
+  <div class="flex-item"><span> 4 </span></div>
+  <div class="flex-item"><span> 5 </span></div>
+  <div class="flex-item"><span> 6 </span></div>
+  <div class="flex-item"><span> 7 </span></div>
+  <div class="flex-item"><span> 8 </span></div>
+  <div class="flex-item"><span> 9 </span></div>
+  <div class="flex-item"><span> 10 </span></div>
+  <div class="flex-item"><span> 11 </span></div>
+  <div class="flex-item"><span> 12 </span></div>
+  <div class="flex-item"><span> 13 </span></div>
+  <div class="flex-item"><span> 14 </span></div>
+  <div class="flex-item"><span> 15 </span></div>
 </div>
