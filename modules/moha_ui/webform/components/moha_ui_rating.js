8f516b18e5bbea702870f363434cbf75bdab251c
Jun Ni <tweeg.ni@gmail.com>
2019-06-25 10:13:00 +0800

    moha_survey 7.x-1.0-dev
    - #4 NPS Support Background.

--- modules/moha_ui/webform/components/moha_ui_rating.js
+++ modules/moha_ui/webform/components/moha_ui_rating.js
@@ -1,12 +1,34 @@
-jQuery(document).ready(function () {
+/**
+ * @file
+ */
+/* jshint esversion: 6 */
+/* jshint unused:false, strict: false */
+/* globals Drupal:object, jQuery:object */
 
-  'use strict';
+(function ($) {
+  Drupal.behaviors.mohaUIRating = {
 
-  /* globals Drupal:object. */
-  Drupal.behaviors.moha_ui_rating = {
-    attach: function (context, settings) {
+    /**
+     * Attach behaviors to enhance UIs.
+     *
+     * @param context document
+     * @param settings Drupal.settings
+     *
+     * @see Drupal.attachBehaviors JSDoc.
+     */
+    attach: function(context, settings) {
+      $('body', context).once('moha_ui_rating', function () {
+        $('#edit-nps', context).change(function () {
+          if ($(this).is(':checked')){
+            const $extra_options = $(this).parent().parent();
+            $extra_options.find('#edit-max-score').val(10);
+            $extra_options.find('#edit-threshold-score').val(6);
+          }
+        });
 
-    },
+        // once finished.
+      });
+    }
   };
 
-});
+})(jQuery);
