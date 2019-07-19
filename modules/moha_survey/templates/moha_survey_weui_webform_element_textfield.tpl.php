048c4e084ca4ee76bd027dfdb218bfabd79a22db
Art Dev <art.dev@tanan.net.cn>
2019-07-19 15:27:19 +0800

    moha_survey 7.x-1.0-dev remove the maxlength limit of textfield

--- modules/moha_survey/templates/moha_survey_weui_webform_element_textfield.tpl.php
+++ modules/moha_survey/templates/moha_survey_weui_webform_element_textfield.tpl.php
@@ -19,7 +19,6 @@
       <input class="weui-input" type="text" placeholder="请输入文本"
         id="<?php print $element['#id'] ?>" name="<?php print $element['#name'] ?>"
         value="<?php print $element['#value'] ?>" size="<?php print $element['#size'] ?>"
-        maxlength="<?php  print $element['#maxlength'] ?>"
       >
     </div>
   </div>
