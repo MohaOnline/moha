5bb4b4020816ef6cc17a51556f2eb00c1e4ad2f6
Jun Ni <tweeg.ni@gmail.com>
2019-07-25 03:59:26 +0800

    moha 7.x-1.0-dev
    - #21 let dashboard works in IE8.

--- modules/moha_ui/templates/html--admin--moha--dashboard.tpl.php
+++ modules/moha_ui/templates/html--admin--moha--dashboard.tpl.php
@@ -52,13 +52,6 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 <body class="skin-blue sidebar-mini <?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
